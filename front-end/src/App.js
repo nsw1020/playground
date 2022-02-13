339fe3e6ed75ee710cb95a3cef798d41f53b0664
user <user@AL01962309.local>
2022-02-13 22:45:46 +0900

    first comment

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          LINE Test React Test
+          Learn Test
         </a>
       </header>
     </div>

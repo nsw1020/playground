38776e3b455175ab7441d6f2ad95dee6cf6f25f0
user <user@AL01962309.local>
2022-02-14 14:08:05 +0900

    first comment

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          LINE Project
+          LINE Project 1
         </a>
       </header>
     </div>

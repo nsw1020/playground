4abfa528091acd1f0f902172fcc8fc2593c229d6
nsw1020 <98509240+nsw1020@users.noreply.github.com>
2022-02-14 11:24:13 +0900

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Test Test
+          Learn Test
         </a>
       </header>
     </div>

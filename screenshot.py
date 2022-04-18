5c47fcf0141eecf34933adab2508ea22d1d70cab
Sung-Wuk, Na <98509240+nsw1020@users.noreply.github.com>
2022-04-18 14:38:38 +0900

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -19,13 +19,12 @@ options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6)
 driver = webdriver.Chrome('/Users/user/Downloads/chromedriver')
 
 # url을 이용하여 브라우저로 접속
-url='https://splunk.linecorp.com/ko-KR/app/search/csirt_ddos_status?form.zone=*&form.message=*&form.ip=*&form.field3.earliest=-4h%40m&form.field3.latest=now'
+url='#url#'
 driver.get(url)
 driver.implicitly_wait(6)
 
 # 아이디/비밀번호를 입력해준다.
-driver.find_element_by_name('username').send_keys('lp12845')
-driver.find_element_by_name('password').send_keys('na6994!@')
+##
 
 # 로그인 버튼 클릭
 driver.find_element_by_xpath('/html/body/div[2]/div/div/div[1]/form/fieldset/input[1]').click()
@@ -33,7 +32,7 @@ driver.find_element_by_xpath('/html/body/div[2]/div/div/div[1]/form/fieldset/inp
 
 # 화면 캡쳐
 sleep(60)
-driver.get_screenshot_as_file('/Users/user/Downloads/ddos.png')
+driver.get_screenshot_as_file('/Users/user/Downloads/xxx.png')
 driver.quit()
 
 
